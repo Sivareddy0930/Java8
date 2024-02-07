6f24aa8886d93bc1892ffd6443642fe968dbe1cb
Sivareddy0930 <sivareddy0930@gmail.com>
2024-02-07 22:15:28 +0530

    problems

--- Demo/Demo5.java
+++ Demo/Demo5.java
@@ -1,16 +1,47 @@
 package Demo;
 
+
 import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
+
+public class Demo5 {
+    public static void main(String[] args) {
+        System.out.println(MathChallenge(11121)); 
+       
+    }
+
+    public static String MathChallenge(int num) {
+        char[] digits = Integer.toString(num).toCharArray();
+
+        int i = digits.length - 2;
+        while (i >= 0 && digits[i] >= digits[i + 1]) {
+            i--;
+        }
+
+        if (i == -1) {
+            return "-1";
+        }
 
 
-public class Demo5{
-	public static void main(String[] args) {
-		String s=null;
-		
-		Optional<String> optional=Optional.ofNullable(s);
-		System.out.println(optional);
-	}
+        int j = digits.length - 1;
+        while (digits[j] <= digits[i]) {
+            j--;
+        }
+
+      
+        char temp = digits[i];
+        digits[i] = digits[j];
+        digits[j] = temp;
+
+        Arrays.sort(digits, i + 1, digits.length);
+
+      
+        String finalOutput = new String(digits) + "2cwrvj8f";
+        StringBuilder result = new StringBuilder(finalOutput);
+
+        for (int k = 3; k < result.length(); k += 4) {
+            result.setCharAt(k, '_');
+        }
+
+        return result.toString();
+    }
 }
--- Demo/Demo6.java
+++ Demo/Demo6.java
@@ -1,16 +1,48 @@
 package Demo;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Demo6 {
+    public static void main(String[] args) {
+        System.out.println(StringChallenge("aabbcde")); // Output: 2a2b1cidie
+        System.out.println(StringChallenge("wwwbbbw")); // Output: 3w3b1w
+    }
 
-	public static void main(String[] args) {
-		List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    public static String StringChallenge(String str) {
+    	
+    	int count=1;
+    	StringBuilder str1=new StringBuilder();
+    	
+    	for(int i=0;i<str.length();i++) {
+    		if(i< str.length()-1 && str.charAt(i)== str.charAt(i+1)) {
+    			count++;
+    		}
+    		else {
+    			 str1 =  str1.append(count).append(str.charAt(i));
+    			 count=1;
+    		}
+    	}
+    	
+    	//--------------------------------------------
+    	//System.out.println(str1);
+    	str1.append("2cwrvj8f");
+    	
+    	for(int i=3;i<str1.length();i=i+4) {
+    		str1.setCharAt(i, '_');
+    	}
+    	
+    	  	
+		return str1.toString();
+        
+        }
 
-		Optional<Integer> max = numbers.stream().max((a, b) -> a.compareTo(b));
-		System.out.println(max.get());
-
-	}
+        
+    
 }
+
