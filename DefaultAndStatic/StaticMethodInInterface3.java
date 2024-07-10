47a3f0213c6c158fb71848cbfd9dd0952e72698c
Sivareddy0930 <sivareddy0930@gmail.com>
2024-07-10 23:33:46 +0530

    Java 8

--- DefaultAndStatic/StaticMethodInInterface3.java
+++ DefaultAndStatic/StaticMethodInInterface3.java
@@ -1,6 +1,6 @@
 package DefaultAndStatic;
 
-interface Test{
+interface StaticTest{
 	static void m1() {
 		System.out.println("This is static method m1 of Interface of Test");
 	}
@@ -8,15 +8,17 @@ interface Test{
 		System.out.println("This is static method m2 of Interface of Test");
 	}
 }
-public class StaticMethodInInterface implements Test {
+public class StaticMethodInInterface3 implements StaticTest {
 
 	public static void main(String[] args) {
 		//we can only access using interfaceName.
+		
+		StaticMethodInInterface3 obj =new StaticMethodInInterface3();
+//		obj.m1(); //we cannot access static methods with object.
 		Test.m1();
 		Test.m2();
-		
+//		m1();// we cannot access static method directly as well....		
 		
 
 	}
-
 }
